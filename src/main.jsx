7678d7fe2b700b7fa9c0361f57bde104f0c0041f
kishore-s-n <snksnk33322@gmail.com>
2025-02-20 23:26:53 +0530

    netlify: github pages are not easy to setup so netlify

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
-import ProjectDetail from './components/ProjectDetail.jsx'
-import ScrollToTop from './components/ScrollToTop.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import ProjectDetail from "./components/ProjectDetail.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
+import "./index.css";
 const Layout = () => {
   return (
     <>
@@ -24,9 +24,8 @@ const router = createBrowserRouter([
   },
 ]);
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/itskishore",
 });
